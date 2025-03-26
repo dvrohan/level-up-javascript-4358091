d44da1a2ba90a5af96875271923c1bfb0be5cb46
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 16:01:47 +0000

    Challenge 14 completed

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,10 @@
 // Write your code here
+function urlify(blogTitle) {
+  const punctuation = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g;
+
+  const blogTitleWithoutPunctuation = blogTitle.replace(punctuation, "");
+  return blogTitleWithoutPunctuation.toLowerCase().trim().replaceAll(" ", "-");
+}
+
+console.log(urlify("How I got into Programming!!!"));
+console.log(urlify("I've got a new job: "))
