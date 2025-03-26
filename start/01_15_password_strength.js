2bd49a2f94687e8847359f88ad425ab1f2bb4790
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 16:11:13 +0000

    Challenge 15 completed.

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,18 @@
 // Write your code here
+function isStrongPassword(password) {
+  let oneLowerCase = /(?=.*[a-z])/;
+  let oneUpperCase = /(?=.*[A-Z])/;
+  let oneNumber = /(?=.*[0-9])/;
+  let oneSpecialCharacter = /(?=.*[!@#$%^&*])/;
+  let minimumEightCharacter = /(?=.{8,})/;
+  let isValid = oneLowerCase.test(password) &&
+    oneUpperCase.test(password) &&
+    oneNumber.test(password) &&
+    oneSpecialCharacter.test(password) &&
+    minimumEightCharacter.test(password);
+
+  return isValid ? "Your password in valid" : "Your password is invalid";
+}
+
+console.log(isStrongPassword("dad"));
+console.log(isStrongPassword("Abcd123*"));
