85559740f5fddad8c161db9a0ae927c1256261a2
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 01:26:43 +0000

    Challenge 7 completed.

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
 // Write your code here
+
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  }
+  else {
+    [teacher, ...students] = classList;
+  }
+
+  return students;
+}
+
+console.log(getStudents({ hasTeachingAssistant: true, classList: ['Teacher', 'TA', 'std1', 'std2'] }))
+console.log(getStudents({ hasTeachingAssistant: false, classList: ['Teacher', 'std1', 'std2'] }))
