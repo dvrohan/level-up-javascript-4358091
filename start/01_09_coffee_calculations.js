901a2b3f237fe04ac7949bedbeb32ec5ee7fabf0
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 14:35:16 +0000

    Challenge 9 completed

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,9 @@
 // Write your code here
+function calculateBill(coffeeArray) {
+  let coffeeSum = coffeeArray.reduce(
+    (totalCoffees, numCoffees) => (totalCoffees += numCoffees)
+  )
+  return `The total bill is $${coffeeSum * 1.25}`;
+}
+
+console.log(calculateBill([2, 5, 7, 1, 4]));
