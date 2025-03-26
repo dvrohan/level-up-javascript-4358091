dfb38d2de0c8cfda39bcef85b8666f194ef6dc7c
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 01:08:43 +0000

    Challenge 2 completed

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director,
+      this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  };
+};
+
+Movie.prototype.getOverview = function () {
+  return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+};
+
+const transformers = new Movie("Transformers", "Michael Bay", "Sci-Fi", 2015, 4);
+
+console.log(transformers.getOverview());
