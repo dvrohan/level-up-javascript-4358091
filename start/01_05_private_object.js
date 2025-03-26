a29d162c019a57d2fbb22b3b9a725a087c1d5b5f
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 01:15:21 +0000

    Challenge 5 completed

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+const username = Symbol("username");
+const password = Symbol("password")
+
+const user = {
+  [username]: "user",
+  [password]: "pass",
+  age: 10
+}
+
+console.log(user.age);
+console.log(user.username);
