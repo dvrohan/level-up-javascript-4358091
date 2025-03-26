bc7d13ac97a2c994d73fb594ef677016115cb030
Rohan Dhamale <47937997+dvrohan@users.noreply.github.com>
2025-03-26 15:41:13 +0000

    Challenge 12 completed

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,22 @@
 // Write your code here
+function getPermutations(string) {
+  let total;
+  for (let i = 1; i <= string.length; i++) {
+    if (total == undefined) {
+      total = i;
+      continue;
+    }
+    total *= i;
+  }
+  return total;
+}
+
+function getPermutationsRecursive(string) {
+  if (string.length === 1) {
+    return 1;
+  }
+  return string.length * getPermutationsRecursive(string.slice(1));
+}
+
+console.log(getPermutations("hello"));
+console.log(getPermutationsRecursive("hello"));
